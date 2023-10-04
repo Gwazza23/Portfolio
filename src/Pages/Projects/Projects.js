a81ff2f57dc1066dff09e55edae4178e5c53ea04
Gwazza23 <weerawarnagayan@gmail.com>
2023-10-04 01:15:24 +0100

    Create Projects Component

---
+++ src/Pages/Projects/Projects.js
@@ -0,0 +1,55 @@
+import { projects } from "../../Util/projects";
+import { VscLinkExternal } from "react-icons/vsc";
+import { AiFillGithub, AiFillExclamationCircle } from "react-icons/ai";
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <div className="projects-container">
+      <div className="projects-content-wrapper">
+        <h2>Projects</h2>
+        <ul className="projects-section-wrapper">
+          {projects.map((project) => {
+            return (
+              <li className="project-container" key={project.id}>
+                <img src={project.img} alt={project.name} />
+                <div className="project-info">
+                  <div className="info-header">
+                    <div className="info-metadata">
+                      <h3>{project.name}</h3>
+                      <p>- {project.created}</p>
+                      {project.note ? (
+                        <div className="exclamation-container">
+                          <AiFillExclamationCircle id="exclamation" />
+                          <p className="pop-up">{project.note}</p>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                    <div className="external-links">
+                      <a href={project.url} rel="noreferrer" target="blank_">
+                        <VscLinkExternal />
+                      </a>
+                      <a href={project.git} rel="noreferrer" target="blank_">
+                        <AiFillGithub />
+                      </a>
+                    </div>
+                  </div>
+                  <p>{project.desc}</p>
+                  <div className="project-stack">
+                    {project.stack.map((tool) => {
+                      return <p key={tool}>{tool}</p>;
+                    })}
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
--- src/Util/AllRoutes.js
+++ src/Util/AllRoutes.js
@@ -3,6 +3,7 @@ import { AnimatePresence } from "framer-motion";
 import Nav from "../Pages/Nav/Nav";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
+import Projects from "../Pages/Projects/Projects";
 
 function AllRoutes() {
   const location = useLocation();
@@ -12,6 +13,7 @@ function AllRoutes() {
         <Route path="/" element={<Nav />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
         </Route>
       </Routes>
     </AnimatePresence>
